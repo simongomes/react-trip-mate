bc9aecd3a6c3d603f502a7c133c9cbc77159b59c
Simon Gomes <busy.s.simon@gmail.com>
2022-01-17 15:25:03 +0600

    basic setup and necessary packages added

---
+++ src/components/List/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const List = () => {
+  return <h1>List</h1>;
+};
+
+export default List;
---
+++ src/components/PlaceDetails/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaceDetails = () => {
+  return <h1>PlaceDetails</h1>;
+};
+
+export default PlaceDetails;
