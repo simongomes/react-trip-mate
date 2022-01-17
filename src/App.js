13c4611cf975b7b015155cea8b0a4980529ff011
Simon Gomes <busy.s.simon@gmail.com>
2022-01-17 15:35:18 +0600

    Base components and file structure added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { CssBaseline, Grid } from "@mui/material";
 
 import Header from "./components/Header";
+import List from "./components/List/index";
+import Map from "./components/Map";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default App;
